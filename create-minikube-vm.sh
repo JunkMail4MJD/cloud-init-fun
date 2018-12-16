30570cd55c09e271404191cb9d07240f1f6d553d
JunkMail4MJD <JunkMail4MJD@users.noreply.github.com>
2018-12-16 16:47:59 -0500

    create a minikube vm
    this is also NOT secure. Do not use an any production context

---
+++ create-minikube-vm.sh
@@ -0,0 +1,82 @@
+## Check if the script is run with bash as shell interpreter.
+if [ -z "$BASH_VERSION" ] ; then
+   echo 'Boot Info Script needs to be run with bash as shell interpreter.' >&2;
+   exit 1;
+fi
+
+help () {
+   cat <<- HELP
+
+	Create minikube VirtualBox VM Script:
+	-----------------------
+    ./create-minikube-vm <virtual machine name> <cloud image filename.ova>
+
+	HELP
+
+   exit 0;
+}
+parameterCount=$#
+if (( parameterCount != 2 )); then
+    echo "Illegal number of parameters: $parameterCount!\n";
+    help;
+    exit 1;
+fi
+
+vmname=$1
+cloudimage=$2
+
+ssh-keygen -t rsa -C ubuntu -f "$(pwd)"/"$vmname"-key -N ""
+publicKey=$(head -n 1 "$(pwd)"/"$vmname"-key.pub)
+
+cat <<EOF > user-data
+#cloud-config
+users:
+  - name: ubuntu
+    plain_text_passwd: 'password'
+    lock_passwd: False
+    gecos: ubuntu
+    shell: /bin/bash
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    ssh-authorized-keys:
+      - $publicKey
+
+system_info:
+  default_user:
+   name: ubuntu
+   plain_text_passwd: 'password'
+   home: /home/ubuntu
+   groups: [sudo, adm ]
+packages:
+ - docker.io
+runcmd:
+ - "ip addr"
+ - "df -h"
+ - [ update-grub ]
+ - "docker run -d --name thisNginx --network host nginx:1.14-alpine"
+ - "curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+ - "chmod +x ./kubectl"
+ - "sudo mv ./kubectl /usr/local/bin/kubectl"
+ - "curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.31.0/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube"
+EOF
+
+cat <<EOF > meta-data
+instance-id: id-12346
+local-hostname: $vmname
+EOF
+
+cloud-localds config-data.iso user-data meta-data
+
+VBoxManage import "$cloudimage" --vsys 0 --vmname "$vmname" --cpus 2 --memory 4096
+
+VBoxManage modifyvm "$vmname" --uart1 0x03f8 4 --uartmode1 file "$(pwd)"/"$vmname"-output.txt
+
+VBoxManage storageattach "$vmname" \
+    --storagectl "IDE" --port 1 --device 0 \
+    --type dvddrive --medium "$(pwd)"/config-data.iso
+## --bridgeadapter<1-N> none|<devicename>
+
+VBoxManage startvm "$vmname"
+
+printf "When your new machine has finished booting,\n"
+printf "ssh into your new box with the following command:\n\n"
+printf "   ssh ubuntu@$vmname -i "$(pwd)"/"$vmname"-key\n\n"
