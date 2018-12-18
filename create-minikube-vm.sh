83dff69c8b5a609069593c3009d42c27f1497605
Mike Dunn <junkmail4mjd@gmail.com>
2018-12-18 01:08:57 -0500

    basic vm with minikube installed

--- create-minikube-vm.sh
+++ create-minikube-vm.sh
@@ -1,41 +1,39 @@
-## Check if the script is run with bash as shell interpreter.
-if [ -z "$BASH_VERSION" ] ; then
-   echo 'Boot Info Script needs to be run with bash as shell interpreter.' >&2;
-   exit 1;
-fi
-
+#!/bin/bash
 help () {
-   cat <<- HELP
+  cat <<- END
+	HELP: Script to create VirtualBox VMs using cloud-init (similar to AWS EC2)
+  ------------------------------------------
+    ./create-generic-vm.sh  <virtual machine name> <cloud image appliance file - ova>
 
-	Create minikube VirtualBox VM Script:
-	-----------------------
-    ./create-minikube-vm <virtual machine name> <cloud image filename.ova>
+    Example: create-generic-vm.sh mini-01 ubuntu-18.04-server-cloudimg-amd64.ova
 
-	HELP
-
-   exit 0;
+	END
 }
 parameterCount=$#
 if (( parameterCount != 2 )); then
-    echo "Illegal number of parameters: $parameterCount!\n";
-    help;
-    exit 1;
+  RED="\033[1;31m"
+  RESET="\033[0;0m"
+  printf "\n${RED}Illegal number of parameters: ${parameterCount}!\n\n${RESET}------------------------------------------\n";
+  help;
+  exit 1;
 fi
+
+echo "Creating virtual machine ${1} based on ${2}"
 
 vmname=$1
 cloudimage=$2
 
-ssh-keygen -t rsa -C ubuntu -f "$(pwd)"/"$vmname"-key -N ""
-publicKey=$(head -n 1 "$(pwd)"/"$vmname"-key.pub)
+mkdir -p "$(pwd)/${vmname}"
+ssh-keygen -t rsa -C ubuntu -f "$(pwd)/${vmname}/${vmname}"-key -N ""
+publicKey=$(head -n 1 "$(pwd)/${vmname}/${vmname}"-key.pub)
 
-cat <<EOF > user-data
+cat <<EOF > "${vmname}/user-data"
 #cloud-config
 users:
   - name: ubuntu
     plain_text_passwd: 'password'
     lock_passwd: False
     gecos: ubuntu
-    shell: /bin/bash
     sudo: ALL=(ALL) NOPASSWD:ALL
     ssh-authorized-keys:
       - $publicKey
@@ -45,7 +43,10 @@ system_info:
    name: ubuntu
    plain_text_passwd: 'password'
    home: /home/ubuntu
+   shell: /bin/bash
+   lock_passwd: False
    groups: [sudo, adm ]
+   shell: /bin/bash
 packages:
  - docker.io
 runcmd:
@@ -59,24 +60,25 @@ runcmd:
  - "curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.31.0/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube"
 EOF
 
-cat <<EOF > meta-data
-instance-id: id-12346
-local-hostname: $vmname
+instanceId=$(openssl rand -hex 8)
+cat <<EOF > "${vmname}/meta-data"
+instance-id: ${instanceId}
+local-hostname: ${vmname}
 EOF
 
-cloud-localds config-data.iso user-data meta-data
+docker run --rm -d --name cloud-init-creator -v $(pwd)/${vmname}/:/usr/src/files junkmail4mjd/cloud-init-creator:v0.0.1
 
-VBoxManage import "$cloudimage" --vsys 0 --vmname "$vmname" --cpus 2 --memory 4096
+VBoxManage import "$cloudimage" --vsys 0 --vmname "${vmname}" --cpus 1 --memory 2048
 
-VBoxManage modifyvm "$vmname" --uart1 0x03f8 4 --uartmode1 file "$(pwd)"/"$vmname"-output.txt
+VBoxManage modifyvm "${vmname}" --uart1 0x03f8 4 --uartmode1 file "$(pwd)/${vmname}/${vmname}"-output.txt
 
-VBoxManage storageattach "$vmname" \
+VBoxManage storageattach "${vmname}" \
     --storagectl "IDE" --port 1 --device 0 \
-    --type dvddrive --medium "$(pwd)"/config-data.iso
+    --type dvddrive --medium "$(pwd)/${vmname}/config-data.iso"
 ## --bridgeadapter<1-N> none|<devicename>
 
-VBoxManage startvm "$vmname"
+VBoxManage startvm "${vmname}"
 
 printf "When your new machine has finished booting,\n"
 printf "ssh into your new box with the following command:\n\n"
-printf "   ssh ubuntu@$vmname -i "$(pwd)"/"$vmname"-key\n\n"
+printf "   ssh ubuntu@${vmname} -i $(pwd)/${vmname}/${vmname}-key\n\n"
