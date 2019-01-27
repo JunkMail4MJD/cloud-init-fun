903039035c92363381089f4228d149181aaaf69a
Mike Dunn <junkmail4mjd@gmail.com>
2019-01-27 11:39:26 -0500

    making image download
    making image download idempotent

--- download-common-images.sh
+++ download-common-images.sh
@@ -4,6 +4,8 @@ printf "\nDownloading Ubuntu 16.04 LTS and 18.04 LTS cloud-init disk images\n\n"
 
 curl -Lo            raw-images/ubuntu-16.04-server-cloudimg-amd64.vmdk http://cloud-images.ubuntu.com/releases/16.04/release/ubuntu-16.04-server-cloudimg-amd64-disk1.vmdk
 VBoxManage clonehd  raw-images/ubuntu-16.04-server-cloudimg-amd64.vmdk images/ubuntu-16.04-server-cloudimg-amd64.vdi --format vdi
+vboxmanage closemedium disk {00000000-0000-0000-0000-000000000000} --delete
 
 curl -Lo            raw-images/ubuntu-18.04-server-cloudimg-amd64.vmdk http://cloud-images.ubuntu.com/releases/18.04/release/ubuntu-18.04-server-cloudimg-amd64.vmdk
 VBoxManage clonehd  raw-images/ubuntu-18.04-server-cloudimg-amd64.vmdk images/ubuntu-18.04-server-cloudimg-amd64.vdi --format vdi
+vboxmanage closemedium disk {00000000-0000-0000-0000-000000000000} --delete
